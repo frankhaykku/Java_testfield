2e8e21d3d86c1076bb4c062ca3a836881379ef3c
unknown <golubnichij_sa@88w684340.kn.loc>
2015-02-25 09:17:57 +0300

    Скелет проекта консольной игры 'угадай число'

---
+++ Guess_Game/src/game/Game.java
@@ -0,0 +1,12 @@
+package game;
+
+/**
+ * Created by golubnichij_sa on 25.02.15.
+ */
+public class Game {
+    private String hello;
+
+    public Game() { this.hello = "Hello, World!"; }
+
+    public String toString() { return this.hello; }
+}
---
+++ Guess_Game/src/main/Solution.java
@@ -0,0 +1,11 @@
+package main;
+
+import game.Game;
+
+public class Solution {
+    public static void main(String[] args) {
+        Game game = new Game();
+
+        System.out.println("game = " + game);
+    }
+}
